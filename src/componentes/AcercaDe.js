4c5a40ba2d14e2aeb3d3a15d1dfe569f07b0a447
Rodrigo Ledesma <rodrigo_ledesma_garcia@outlook.com>
2024-01-18 22:29:24 -0600

    first commit

---
+++ src/componentes/AcercaDe.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const AcercaDe = () => {
+    return ( 
+        <div>
+              <h2>ACERCA DE</h2>
+              <p>Hola mundo</p>
+          </div>
+     );
+}
+ 
+export default AcercaDe;
---
+++ src/componentes/Inicio.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Inicio = () => {
+    return ( 
+        <div>
+            <h2>Pagina de Inicio</h2>
+                <p>Esta es la pagina principal de mi sitio</p>
+      </div>
+     );
+}
+ 
+export default Inicio;
